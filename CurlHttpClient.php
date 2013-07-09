a65dcac7a5fdcb4ba9952a531d74ba58b5caca4b
Charles Tang <charlestang@foxmail.com>
2013-07-09 14:18:49 +0800

    init the curl instance.

--- CurlHttpClient.php
+++ CurlHttpClient.php
@@ -2,14 +2,37 @@
 
 /**
  * This is a wrapper class of cURL extension of php
+ * @author Charles Tang<charlestang@foxmail.com>
  */
 class CurlHttpClient {
 
     public static $version = 1.0;
     private static $_curlHttpClient = null;
+    private static $_defaultOptions = array(
+        CURLOPT_RETURNTRANSFER => true,  //return the result
+        CURLOPT_SSL_VERIFYPEER => false, //ignore SSL
+        CURLOPT_TIMEOUT        => 8,     //default timeout 
+        CURLOPT_USERAGENT      => 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1) Gecko/20100101 Firefox/22.0',
+    );
 
+    /**
+     * cURL instance
+     * @var resource 
+     */
+    private $_curl = null;
+
+    /**
+     * Constructor
+     * @throws Exception
+     */
     public function __construct() {
-        
+        $this->_curl = curl_init();
+        if ($this->_curl === false) {
+            throw new Exception("cURL init failed.");
+        }
+        if (!curl_setopt_array($this->_curl, self::$_defaultOptions)) {
+            throw new Exception("cURL default options set failed.");
+        }
     }
 
     /**
