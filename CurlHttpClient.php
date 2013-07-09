aa945aed77368de64d9f00c7e59c853312972ec1
Charles Tang <charlestang@foxmail.com>
2013-07-09 16:57:02 +0800

    how curl-http-client do requests.

--- CurlHttpClient.php
+++ CurlHttpClient.php
@@ -26,9 +26,29 @@ class CurlHttpClient {
     private $_options = array();
 
     /**
+     * @var string the request uri 
+     */
+    private $_url = '';
+
+    /**
      * @var array the headers container 
      */
     private $_headers = array();
+
+    /**
+     * @var array the cookies container 
+     */
+    private $_cookies = array();
+
+    /**
+     * @var array the request params container 
+     */
+    private $_queries = array();
+    private $_errorCode = 0;
+    private $_errorMsg = '';
+    private $_responseHeaderStr = '';
+    private $_responseBody = '';
+    private $_responseHeader = array();
 
     /**
      * Constructor
@@ -40,7 +60,15 @@ class CurlHttpClient {
 
     public function init() {
         $this->_options = array();
+        $this->_url = '';
         $this->_headers = array();
+        $this->_cookies = array();
+        $this->_queries = array();
+        $this->_errorCode = 0;
+        $this->_errorMsg = '';
+        $this->_responseHeaderStr = '';
+        $this->_responseBody = '';
+        $this->_responseHeader = array();
         $this->_curl = curl_init();
         if ($this->_curl === false) {
             throw new Exception("cURL init failed.");
@@ -58,21 +86,107 @@ class CurlHttpClient {
         $this->init();
     }
 
+    protected function doRequest() {
+        curl_setopt_array($this->_curl, $this->_options);
+
+        //set headers information
+        if (!empty($this->_headers)) {
+            $headers = array();
+            foreach ($this->_headers as $key => $value) {
+                array_push($headers, "$key: $value");
+            }
+            curl_setopt($this->_curl, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        //set cookies information
+        if (!empty($this->_cookies)) {
+            $cookies = '';
+            foreach ($this->_cookies as $key => $value) {
+                $cookies .= "$key=$value; ";
+            }
+            $cookies = trim($cookies, '; ');
+            curl_setopt($this->_curl, CURLOPT_COOKIE, $cookies);
+        }
+
+        curl_setopt($this->_curl, CURLOPT_URL, $this->_url);
+        curl_setopt($this->_curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->_curl, CURLOPT_HEADER, true);
+
+        $this->_errorCode = 0;
+        $this->_errorMsg = '';
+        $this->_responseHeaderStr = '';
+        $this->_responseBody = '';
+        
+        $ret = curl_exec($this->_curl);
+        if (false !== $ret) {
+            $response = explode("\r\n", $ret, 2);
+            $this->_responseHeaderStr = $response[0];
+            $this->_responseBody = $response[1];
+        } else {
+            $this->_errorCode = curl_errno($this->_curl);
+            $this->_errorMsg = curl_error($this->_curl);
+        }
+
+        return $this->_errorCode === 0;
+    }
+
+    public function getRequest($url) {
+        $this->_url = $url;
+        $queryStr = '';
+        if (!empty($this->_queries)) {
+            $queryStr = http_build_query($this->_queries);
+        }
+
+        if (strpos('?', $url) === false) {
+            $this->_url .= '?' . $queryStr;
+        } else {
+            $this->_url .= '&' . $queryStr;
+        }
+
+        if ($this->doRequest()) {
+            return $this->_responseBody;
+        } 
+    }
+
+    public function postRequest($url) {
+        $this->_url = $url;
+        curl_setopt($this->_curl, CURLOPT_POST, true);
+        if (!empty($this->_queries)) {
+            curl_setopt($this->_curl, CURLOPT_POSTFIELDS, $this->_queries);
+        }
+
+        if ($this->doRequest()) {
+            return $this->_responseBody;
+        }
+    }
+
     /**
      * Set the cookie
      * @param mixed $cookies the cookies can be string like "foo:bar; foo2:bar2" or a array('foo' => 'bar', 'foo2' => 'bar2');
      */
     public function setCookies($cookies) {
-        $cookiesStr = $cookies;
         if (is_array($cookies)) {
-            $cookiesStr = '';
             foreach ($cookies as $key => $value) {
-                $cookiesStr .= "$key=$value; "; //joined them by a simecolon followed by a space
+                $this->setCookie($key, $value);
             }
-            $cookiesStr = trim($cookiesStr, '; ');
         }
 
-        $this->_options[CURLOPT_COOKIE] = $cookiesStr;
+        if (is_string($cookies)) {
+            $cookieStrs = explode(';', $cookies);
+            foreach ($cookieStrs as $cookie) {
+                $pair = explode('=', $cookie);
+                $this->setCookie(trim($pair[0]), $pair[1]);
+            }
+        }
+    }
+
+    /**
+     * Set single cookie entry
+     * @param string $key
+     * @param string $value
+     */
+    public function setCookie($key, $value) {
+        $this->_cookies[$key] = $value;
     }
 
     /**
@@ -94,6 +208,29 @@ class CurlHttpClient {
      */
     public function setHeader($header, $value) {
         $this->_headers[$header] = $value;
+    }
+
+    /**
+     * @param mixed $queries query string or array 
+     */
+    public function setQueries($queries) {
+        if (is_array($queries)) {
+            foreach ($queries as $key => $value) {
+                $this->setQuery($key, $value);
+            }
+        }
+
+        if (is_string($queries)) {
+            $queryStrs = explode('&', $queries);
+            foreach ($queryStrs as $query) {
+                $pair = explode('=', $query, 2);
+                $this->setQuery(trim($pair[0]), $pair[1]);
+            }
+        }
+    }
+
+    public function setQuery($key, $value) {
+        $this->_queries[$key] = $value;
     }
 
     /**
